f835b6fca2a50d52d033acfd83adfb8fc126e332
WinterWolfSV <what112222@gmail.com>
2023-11-05 22:30:02 +0100

    05/11/23

---
+++ python_code/cube_solver.py
@@ -0,0 +1,52 @@
+speed = 250  # Speed for the motor to rotate in rpm
+
+cube_mapping = {
+    "F": 0,
+    "L": 1,
+    "B": 2,
+    "R": 3,
+    "U": 4,
+}
+
+move_d_prime = "R2 L2 U F2 B2 U F2 R2 F2 B2 U2 L2 U2 L2 R2 U2 R2 U2 R2 F2 U' R2 B2 R2 L2 F2 L2 U B2 F2 U"
+
+
+def turn_faces(moves):
+    # Split the moves into a list
+    moves = moves.split(" ")
+    # Create a list of commands
+    commands = []
+    for move in moves:
+        print(move)
+        if "D'" in move:
+            for command in turn_faces(move_d_prime):
+                commands.append(command)
+            continue
+        elif "D2" in move:
+            for command in turn_faces(move_d_prime + " " + move_d_prime):
+                commands.append(command)
+            continue
+        elif "D" in move:
+            tempmove = move_d_prime[::-1].split(" ")
+            tempmove = " ".join(tempmove)
+            for command in turn_faces(tempmove):
+                commands.append(command)
+            continue
+
+        commands.append(turn_face(move))
+    return commands
+
+
+def turn_face(move):
+    # If move contains 2, remove it and add 180 to the rotation
+    rotation = 90
+    if "2" in move:
+        rotation = 180
+    elif "'" in move:
+        rotation = -90
+
+    # Remove the 2 or ' from the move
+    face = move.replace("2", "").replace("'", "")
+    motor_num = cube_mapping[face]
+
+    return f"rotate {motor_num} {rotation} {speed}"
---
+++ python_code/main_program.py
@@ -0,0 +1,29 @@
+import serial
+import time
+import cube_solver
+
+# from rubik_solver import utils
+# cube = 'wowgybwyogygybyoggrowbrgywrborwggybrbwororbwborgowryby'
+# print(utils.solve(cube, 'Kociemba'))
+
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.5)
+
+scamble = "R U R' U R U2 R' U"
+
+
+
+
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    # data = arduino.readall()
+    # return data
+
+# B2 U2 L' B' R2 U2 D2 B L2 R' U2 F B' R' D' R B' F' U' B2
+while True:
+    user_input = input("Enter a move: ")  # Taking input from user
+    moves = cube_solver.turn_faces(user_input)
+    print(moves)
+    for move in moves:
+        print(move)
+        write_read(move)
+        time.sleep(0.5)
