f7f93cbc914bd4e9a100ed9c50a4ffaed7a0e697
WinterWolfSV <what112222@gmail.com>
2023-12-11 11:42:19 +0100

    Update driver to support tmc2208

--- arduino_code/main_driver.ino
+++ arduino_code/main_driver.ino
@@ -4,29 +4,29 @@
 #include "A4988.h"
 
 // Define the number of stepper motors
-const int numMotors = 5;
+const int numMotors = 6;
 
 // Define pin arrays for direction and step pins
-int DirPins[numMotors] = { 35, 37, 39, 41, 43 };
-int StepPins[numMotors] = { 45, 47, 49, 51, 53 };
-int SleepPins[numMotors] = { 23, 25, 27, 29, 31 };
+int DirPins[numMotors] = { 23, 25, 27, 29, 31, 33 };
+int StepPins[numMotors] = { 22, 24, 26, 28, 30, 32 };
+int SleepPins[numMotors] = { 41, 43, 45, 47, 49, 51 };
 
-int MS1 = 26;    //GPIO13---D7 of Nodemcu--MS1 for A4988
-int MS2 = 24;    //GPIO16---D0 of Nodemcu--MS2 for A4988
-int MS3 = 22;    //GPIO15---D8 of Nodemcu--MS3 for A4988
-int Sleep = 23;  //GPIO12---D6 of Nodemcu-Control Sleep Mode on A4988
+int MS1 = 35;    //GPIO13---D7 of Nodemcu--MS1 for A4988
+int MS2 = 37;    //GPIO16---D0 of Nodemcu--MS2 for A4988
+int MS3 = 39;    //GPIO15---D8 of Nodemcu--MS3 for A4988
 int rpm = 300;
-int microsteps = 4;
+int microsteps = 8;
 int commandData[3];
 
 // Define an array of A4988 stepper motor objects
 //A4988 stepper(200, 35, 45, MS1, MS2, MS3);
 A4988 steppers[numMotors] = {
-  A4988(200, DirPins[0], StepPins[0], MS1, MS2, MS3),
+  A4988(200, DirPins[0], StepPins[0], 0,0,0),
   A4988(200, DirPins[1], StepPins[1], MS1, MS2, MS3),
   A4988(200, DirPins[2], StepPins[2], MS1, MS2, MS3),
   A4988(200, DirPins[3], StepPins[3], MS1, MS2, MS3),
-  A4988(200, DirPins[4], StepPins[4], MS1, MS2, MS3)
+  A4988(200, DirPins[4], StepPins[4], MS1, MS2, MS3),
+  A4988(200, DirPins[5], StepPins[5], MS1, MS2, MS3)
 };
 
 void setup() {
@@ -34,8 +34,11 @@ void setup() {
   Serial.println("Hello World!");
 
   for (int i = 0; i < numMotors; i++) {
-   digitalWrite(SleepPins[i], LOW);
- }
+    pinMode(DirPins[i], OUTPUT);
+    pinMode(StepPins[i], OUTPUT);
+    pinMode(SleepPins[i], OUTPUT);
+    digitalWrite(SleepPins[i], HIGH);
+  }
 }
 
 int localMotor = 0;
@@ -55,7 +58,7 @@ void loop() {
 void rotateMotor(int motor, int degrees, int speed) {
   steppers[motor].begin(speed, microsteps);
   steppers[motor].enable();
-  
+
   steppers[motor].rotate(degrees);
   steppers[motor].disable();
 }
@@ -72,7 +75,7 @@ void getDataFromRotationCommand(String inputString) {
     spaceIndex = inputString.indexOf(' ');
     token = inputString.substring(0, spaceIndex);
     inputString = inputString.substring(spaceIndex + 1);
-    commandData[tokenIndex-1] = token.toInt();
+    commandData[tokenIndex - 1] = token.toInt();
 
 
     switch (tokenIndex) {
@@ -102,9 +105,10 @@ void getDataFromRotationCommand(String inputString) {
   Serial.println(localDegrees);
   Serial.print("Speed: ");
   Serial.println(localSpeed);
-  digitalWrite(SleepPins[localMotor], HIGH);
+  digitalWrite(SleepPins[localMotor], LOW);
   delay(1);
+  Serial.println(steppers[localMotor].getDirection());
   steppers[localMotor].begin(localSpeed, microsteps);
   steppers[localMotor].rotate(localDegrees);
-  digitalWrite(SleepPins[localMotor], LOW);
+  digitalWrite(SleepPins[localMotor], HIGH);
 }
